3d8998ed09c0d3f81fe0aaac3a1a0a69416c0dbe
Stamatis Gounidis <74081279+stam2504@users.noreply.github.com>
2021-12-30 14:44:40 +0200

    Add files via upload

---
+++ QR Code.py
@@ -0,0 +1,12 @@
+import qrcode 
+qr=qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=5
+    
+)
+data="hello stam"
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black", backcolor="white")
+img.save("1.png")
